8acfb89f5c59809b95fcd679af84469cd73f7cd3
Alexsey Funikov <it-kadui@mail.ru>
2022-08-15 17:49:42 +0300

    delete bag

--- src/pages/MainPages.tsx
+++ src/pages/MainPages.tsx
@@ -1,18 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { _defaultNumberChar } from '../common/variables';
 import { Container } from '../components/Cantainer';
 import { CharList } from '../components/CharList';
 import { DescrChar } from '../components/DescrChar';
 import { RandomChar } from '../components/RandomChar';
 
-import { useAppDispatch } from '../hooks/hooks';
+import { useAppDispatch, useAppSelector } from '../hooks/hooks';
 import { getChars } from '../redux/slices/asynkThunks/getChars';
 import { changeOffset } from '../redux/slices/charSlice';
 
 export const MainPages = () => {
   const dispatch = useAppDispatch();
+  const dataChars = useAppSelector((state) => state.char.dataChar);
+
   useEffect(() => {
-    dispatch(getChars());
+    if (!dataChars.length) {
+      dispatch(getChars());
+    }
+
     dispatch(changeOffset(_defaultNumberChar));
   }, [dispatch]);
   return (
